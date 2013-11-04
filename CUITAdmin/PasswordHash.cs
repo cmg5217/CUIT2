63f7f3453668918f9fe12565e88da525ce662272
David Stephens <d.m.stephens@eagle.clarion.edu>
2013-11-04 17:30:28 -0500

    sha512 class added

---
+++ CUITAdmin/PasswordHash.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace CUITAdmin
+{
+    class PasswordHash
+    {
+        //this method returns a SHA512 Hash string
+        public static string getHashSha512(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            SHA512Managed hashstring = new SHA512Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+
+     }
+}
